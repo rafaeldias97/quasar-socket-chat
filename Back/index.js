5d9536b025a74b60268c25f9cd716575ddc88332
Rafael Dias <rafael.cdc97@gmail.com>
2019-01-04 12:41:24 -0200

    :speech_balloon: Chat :speech_balloon:

---
+++ Back/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = express()
+
+const server = app.listen(3001, function () {
+  console.log('server running on port 3001');
+})
+
+const io = require('socket.io')(server)
+
+io.on('connection', function (socket) {
+  console.log(socket.id)
+  socket.on('GENERATE_ID', function (data) {
+    io.emit('MESSAGE_id', { id: socket.id, data: data})
+  })
+  socket.on('SEND_MESSAGE', function (data) {
+    io.emit('MESSAGE', data)
+  })
+})
